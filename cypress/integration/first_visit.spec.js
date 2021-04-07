6042f78ed1eb86cd8e2ace16f4f577eb7dd726d2
Alexandre do Sim <aledosim@yahoo.com.br>
2021-04-07 18:25:23 -0300

    Changed first_visit E2E to reflect CoinLogo changes

--- cypress/integration/first_visit.spec.js
+++ cypress/integration/first_visit.spec.js
@@ -1,28 +1,37 @@
 it('Gus first impressions', () => {
 
-    // Gus wants to know about bitcoin and visits BitInforma home page
-    cy.visit('/')
+  // Gus wants to know about bitcoin and visits BitInforma home page
+  cy.visit('/')
 
-    // He notes the topbar with the BitInforma logo and the search field
-    cy.dataCy('topbar')
-        .should('be.visible')
-        .within(($topbar) => {
+  // He notes the topbar with the BitInforma logo and the search field
+  cy.dataCy('topbar')
+    .should('be.visible')
+    .within(($topbar) => {
 
-            cy.get('input[type=text]')
-                .should('be.visible')
-
-            cy.get('img[alt*=search]')
-                .should('be.visible')
-        })
-
-    // He notes the two cards and the background as well
-    cy.dataCy('infocard')
+      cy.get('input[type=text]')
         .should('be.visible')
 
-    cy.dataCy('infocard24h')
+      cy.get('img[alt*=search]')
+        .should('be.visible')
+    })
+
+  // He notes the two cards and the background as well
+  cy.dataCy('infocard')
+    .should('be.visible')
+    .within(($infocard) => {
+
+      cy.get('header img')
         .should('be.visible')
 
-    cy.dataCy('background')
-        .should('be.visible')
+      cy.get('header > span:last-of-type')
+        .should('contain', 'Bitcoin')
+    })
+
+
+  cy.dataCy('infocard24h')
+    .should('be.visible')
+
+  cy.dataCy('background')
+    .should('be.visible')
 })
 
