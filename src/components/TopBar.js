8e767c0587479d21da0db08908e0c5ffe188a89d
Alexandre do Sim <aledosim@yahoo.com.br>
2021-04-07 14:38:26 -0300

    Implemented closure solution for search function

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,33 +1,53 @@
 import React, { useContext } from 'react';
 
+import { CurrencyContext } from '../contexts/CurrencyContext'
+
 import logo from "../images/logo.svg"
 import search_icon from '../images/search_icon.svg'
 
 import styles from '../styles/components/TopBar.module.css'
+import { configs } from '../configs'
 
-import { CurrencyContext } from '../contexts/CurrencyContext'
+export function search(setCurrency) {
+  const searchInner = () => {
+    const searchField  = document.getElementById('searchField')
+    const text = searchField.value.toLowerCase()
+
+    if (Object.keys(configs.currencies).includes(text)) {
+      setCurrency(text)
+      searchField.value = ''
+    } else {
+      searchField.value = ''
+      searchField.placeholder = 'Termo inválido'
+    }
+  }
+
+  return searchInner
+}
 
 export default function TopBar(){
-    const {
-        search
-    } = useContext(CurrencyContext)
 
-    return(
-        <header data-cy='topbar' className={styles.topBar}>
-            <div>
-                <img className={styles.logo} src={logo} alt='logo'></img>
-            </div>
-            <div className={styles.searchFieldContainer}>
-                <input
-                    type='text'
-                    placeholder='Buscar por uma moeda'
-                />
-                <img
-                    src={search_icon}
-                    alt='search button'
-                    onClick={search}
-                />
-            </div>
-        </header>
-    );
+  const {
+    setCurrency
+  } = useContext(CurrencyContext)
+
+  return(
+    <header data-cy='topbar' className={styles.topBar}>
+      <div>
+        <img className={styles.logo} src={logo} alt='logo'></img>
+      </div>
+      <div className={styles.searchFieldContainer}>
+        <input
+          id='searchField'
+          type='text'
+          placeholder='Buscar por uma moeda'
+        />
+        <img
+          src={search_icon}
+          alt='search button'
+          onClick={search(setCurrency)}
+        />
+      </div>
+    </header>
+  );
 };
--- src/components/TopBar.test.js
+++ src/components/TopBar.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import CurrencyProvider from '../contexts/CurrencyContext'
-import TopBar from './TopBar';
+import TopBar, { search } from './TopBar';
 
-function renderTopBar() {
+function renderTopBar () {
     return render(
         <CurrencyProvider>
             <TopBar />
@@ -12,9 +12,65 @@ function renderTopBar() {
     )
 }
 
+describe('search tests', () => {
+
+  it('returns a function', () => {
+    const func = search(jest.fn())
+
+    expect(typeof func).toEqual('function')
+  })
+
+  it('gets the correct input', () => {
+    document.getElementById = jest.fn(() => HTMLInputElement)
+
+    const func = search(jest.fn())
+    func()
+
+    expect(document.getElementById).toHaveBeenCalledWith('searchField')
+  })
+
+  it('search for lowercase only', () => {
+    HTMLInputElement.value = 'eth'
+    document.getElementById = jest.fn(() => HTMLInputElement)
+    const spy = jest.spyOn(String.prototype, 'toLowerCase')
+
+    const func = search(jest.fn())
+    func()
+
+    expect(String.prototype.toLowerCase).toHaveBeenCalledTimes(1)
+
+    spy.mockRestore()
+  })
+
+  it('calls setCurrency for a valid value and reset searchField', () => {
+    const setCurrency = jest.fn()
+    HTMLInputElement.value = 'eth'
+    document.getElementById = jest.fn(() => HTMLInputElement)
+
+    const func = search(setCurrency)
+    func()
+
+    expect(setCurrency).toHaveBeenCalled()
+    expect(HTMLInputElement.value).toEqual('')
+  })
+
+  it('wont call setCurrency for a invalid value and show a mesage', () => {
+    const setCurrency = jest.fn()
+    HTMLInputElement.value = 'invalid'
+    document.getElementById = jest.fn(() => HTMLInputElement)
+
+    const func = search(setCurrency)
+    func()
+
+    expect(setCurrency).not.toHaveBeenCalled()
+    expect(HTMLInputElement.placeholder).toEqual('Termo inválido')
+    expect(HTMLInputElement.value).toEqual('')
+  })
+})
+
 describe('<TopBar /> tests', () => {
 
     it('renders without crashing', () => {
         renderTopBar()
-    });
+    })
 })
