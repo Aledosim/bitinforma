18a5b61a0d1a4419e14a5b4ccc1321eca53f85ca
Alexandre do Sim <aledosim@yahoo.com.br>
2021-04-07 17:59:12 -0300

    Refactored TopBar.js into two files

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,24 +6,7 @@ import logo from "../images/logo.svg"
 import search_icon from '../images/search_icon.svg'
 
 import styles from '../styles/components/TopBar.module.css'
-import { configs } from '../configs'
-
-export function search(setCurrency) {
-  const searchInner = () => {
-    const searchField  = document.getElementById('searchField')
-    const text = searchField.value.toLowerCase()
-
-    if (Object.keys(configs.currencies).includes(text)) {
-      setCurrency(text)
-      searchField.value = ''
-    } else {
-      searchField.value = ''
-      searchField.placeholder = 'Termo inválido'
-    }
-  }
-
-  return searchInner
-}
+import { search } from '../functions/functions'
 
 export default function TopBar(){
 
