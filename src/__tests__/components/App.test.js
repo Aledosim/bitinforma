bb57b4013d906dba827cb5da446467ddbeb475b2
Alexandre do Sim <aledosim@yahoo.com.br>
2021-04-18 21:42:16 -0300

    App unit test does not use snapshot

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,35 +1,8 @@
-import { enableFetchMocks  } from 'jest-fetch-mock'
-
-enableFetchMocks()
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
-
-import { tickerResponse, summaryResponse } from '../fixtures.json'
+import { render, screen } from '@testing-library/react'
 
 import App from '../../components/App';
 
-beforeEach(() => {
-  fetch.resetMocks()
-
-  fetch
-    .mockResponse(req => {
-      if (/.*\/ticker\/.*/.test(req.url)) {
-        return Promise.resolve({ body: JSON.stringify(tickerResponse) })
-
-      } else if (/.*\/day-summary\/.*/.test(req.url)) {
-        return Promise.resolve({ body: JSON.stringify(summaryResponse) })
-
-      }
-    })
-})
-
-it('renders without crashing', async () => {
-  const tree = render(<App />)
-
-  // Check if the fetch data is rendered
-  await waitFor(() => {
-    expect(fetch).toHaveBeenCalledTimes(2)
-  })
-
-  expect(tree).toMatchSnapshot()
+it('renders without crashing', () => {
+  render(<App />)
 });
