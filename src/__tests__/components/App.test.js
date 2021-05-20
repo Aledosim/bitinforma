934e19ae7b101c86c1c422972ea1b3848233d871
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-20 01:13:27 -0300

    Refactor of tests

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,27 +1,7 @@
-import { enableFetchMocks  } from 'jest-fetch-mock'
-
-enableFetchMocks()
 import React from 'react'
 import { render, waitFor } from '@testing-library/react'
 
-import { tickerResponse, summaryResponse } from '../fixtures.json'
-
 import App from '../../components/App';
-
-beforeEach(() => {
-  fetch.resetMocks()
-
-  fetch
-    .mockResponse(req => {
-      if (/.*\/ticker\/.*/.test(req.url)) {
-        return Promise.resolve({ body: JSON.stringify(tickerResponse) })
-
-      } else if (/.*\/day-summary\/.*/.test(req.url)) {
-        return Promise.resolve({ body: JSON.stringify(summaryResponse) })
-
-      }
-    })
-})
 
 it('renders without crashing', async () => {
   const tree = render(<App />)
